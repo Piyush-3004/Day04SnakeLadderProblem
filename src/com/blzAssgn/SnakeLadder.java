bcc0f3572268ceaca7108abf3987f26d455c0129
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 23:03:07 +0530

    Check Options

--- src/com/blzAssgn/SnakeLadder.java
+++ src/com/blzAssgn/SnakeLadder.java
@@ -4,21 +4,29 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SnakeLadder {
-
 	public static void main(String[] args) {
-
-		int position = 0;
+		// TODO Auto-generated method stub
+		
+		int position=0;
 		System.out.println("This is Start of game you are at position " + position);
 		System.out.println("Enter 1 to roll the dice");
 
 		Scanner sc = new Scanner(System.in);
-		int op = sc.nextInt();
+		int op=sc.nextInt();
 //		System.out.println("Enter 0 to skip dice rolling");
-
+		
 		Random random = new Random();
-		int diceRoll = 1 + random.nextInt(6);
 
-		System.out.println(diceRoll);
+		int diceRoll = 1+ random.nextInt(6);
+			
+		int op1 =  random.nextInt(3);
 
-	}
+		if (op1==1)
+		position = position+ diceRoll;
+		System.out.println(position);
+		
+		if(op1==2)
+			position=position-diceRoll;
+		System.out.println(position);
+		}
 }
