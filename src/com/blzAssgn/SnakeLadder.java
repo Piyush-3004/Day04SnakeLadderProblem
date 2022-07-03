e27a3491b57d86e9f435b7d26523524b5ccbaf7b
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 22:54:13 +0530

    Start Game at position 0

---
+++ src/com/blzAssgn/SnakeLadder.java
@@ -0,0 +1,14 @@
+package com.blzAssgn;
+
+public class SnakeLadder {
+	static public void play() {
+		
+		int position=0;
+		System.out.println("This is Start of game you are at position " + position);	
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SnakeLadder.play();
+	}
+}
