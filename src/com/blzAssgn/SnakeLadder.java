b7fd3ca84b7c5f10bd572f7d82e3ce652252e35e
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 23:05:25 +0530

    Repeat till player position 100

--- src/com/blzAssgn/SnakeLadder.java
+++ src/com/blzAssgn/SnakeLadder.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int position=0;
 		System.out.println("This is Start of game you are at position " + position);
 		System.out.println("Enter 1 to roll the dice");
@@ -17,16 +17,29 @@ public class SnakeLadder {
 		
 		Random random = new Random();
 
+		while(position<100 ) {
 		int diceRoll = 1+ random.nextInt(6);
 			
-		int op1 =  random.nextInt(3);
+		int op1 =  random.nextInt(2);
 
-		if (op1==1)
-		position = position+ diceRoll;
-		System.out.println(position);
 		
-		if(op1==2)
-			position=position-diceRoll;
-		System.out.println(position);
+		if (op1==0) {
+			if(position + diceRoll>100)
+				position = position;
+				else {
+		position = position+ diceRoll;
+		System.out.println(position);}
 		}
+		
+		if(op1==1 && position !=0) {
+			if (position-diceRoll<0)
+				position=0;
+				else {
+		position=position-diceRoll;
+		System.out.println(position);
+				}
+		}
+		
+		}
+	}
 }
