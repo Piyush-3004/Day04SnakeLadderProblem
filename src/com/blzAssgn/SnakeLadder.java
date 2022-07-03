e0aea79e08f67510977a965a1621015286d305db
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 23:09:26 +0530

    Print DiceRoll Count and Position

--- src/com/blzAssgn/SnakeLadder.java
+++ src/com/blzAssgn/SnakeLadder.java
@@ -6,23 +6,19 @@ import java.util.Scanner;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		int position=0;
+		int rollCount=0;
 		System.out.println("This is Start of game you are at position " + position);
 		System.out.println("Enter 1 to roll the dice");
-
 		Scanner sc = new Scanner(System.in);
 		int op=sc.nextInt();
 //		System.out.println("Enter 0 to skip dice rolling");
-		
 		Random random = new Random();
-
 		while(position<100 ) {
 		int diceRoll = 1+ random.nextInt(6);
-			
-		int op1 =  random.nextInt(2);
-
+		rollCount=rollCount+1;
 		
+		int op1 =  random.nextInt(2);
 		if (op1==0) {
 			if(position + diceRoll>100)
 				position = position;
@@ -30,7 +26,6 @@ public class SnakeLadder {
 		position = position+ diceRoll;
 		System.out.println(position);}
 		}
-		
 		if(op1==1 && position !=0) {
 			if (position-diceRoll<0)
 				position=0;
@@ -39,8 +34,7 @@ public class SnakeLadder {
 		System.out.println(position);
 				}
 		}
-		
 		}
+		System.out.println("played  "+rollCount+"  times");
 	}
-
 }
