82567d2e82ed6c525cfaf06b71adb865e7491939
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 22:59:29 +0530

    Dice roll

--- src/com/blzAssgn/SnakeLadder.java
+++ src/com/blzAssgn/SnakeLadder.java
@@ -1,14 +1,24 @@
 package com.blzAssgn;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class SnakeLadder {
-	static public void play() {
-		
-		int position=0;
-		System.out.println("This is Start of game you are at position " + position);	
-		
-	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SnakeLadder.play();
+
+		int position = 0;
+		System.out.println("This is Start of game you are at position " + position);
+		System.out.println("Enter 1 to roll the dice");
+
+		Scanner sc = new Scanner(System.in);
+		int op = sc.nextInt();
+//		System.out.println("Enter 0 to skip dice rolling");
+
+		Random random = new Random();
+		int diceRoll = 1 + random.nextInt(6);
+
+		System.out.println(diceRoll);
+
 	}
 }
